ba354262087a5b7748e7fffb0d685d9e93b32633
Kay Wilhelm Mähler <KWMSources@users.noreply.github.com>
2020-06-24 00:36:44 +0200

    Resource complete

---
+++ client/client.js
@@ -0,0 +1,38 @@
+import { startPedControler } from './pedSyncer/control/PedControler.mjs';
+import { Ped } from './pedSyncer/class/PedClass.mjs';
+import alt from 'alt';
+import native from 'natives';
+
+startPedControler();
+
+alt.everyTick(() => {
+    let playerPos = alt.Player.local.pos;
+    for (let ped of Ped.getAllStreamedPeds()) {
+        let playerPos2 = native.getEntityCoords(ped.scriptID);
+        let distance = Math.round(native.getDistanceBetweenCoords(playerPos.x, playerPos.y, playerPos.z, playerPos2.x, playerPos2.y, playerPos2.z, true));
+
+        let scale = distance / (15 * 15.0);
+        if (scale < 0.25) scale = 0.25;
+
+        let screenPos = native.getScreenCoordFromWorldCoord(playerPos2.x, playerPos2.y, playerPos2.z + 0.94);
+
+        drawNameTags(`pedId: ${ped.id}, ${distance}`, screenPos[1], screenPos[2] - 0.030, 0.3, 255, 255, 255, 220, true);
+    }
+});
+
+function drawNameTags(text, x, y, scale, r, g, b, a, outline) {
+    native.setTextFont(0);
+    native.setTextProportional(0);
+    native.setTextScale(scale, scale);
+    native.setTextColour(r, g, b, a);
+    native.setTextDropShadow(0, 0, 0, 0, 255);
+    native.setTextEdge(2, 0, 0, 0, 255);
+    native.setTextCentre(1);
+    native.setTextDropShadow();
+
+    if (outline) native.setTextOutline();
+
+    native.beginTextCommandDisplayText("STRING");
+    native.addTextComponentSubstringUnk(text);
+    native.endTextCommandDisplayText(x, y);
+} 
---
+++ client/pedSyncer/control/PedControler.mjs
@@ -0,0 +1,89 @@
+import native from 'natives';
+import * as alt from 'alt';
+import { Ped } from '../class/PedClass.mjs';
+
+var pedType = 1654;
+
+export function startPedControler() {
+    //Event which fires if syncedMeta of a ped has been deleted
+    alt.onServer('pedSyncer:server:metaDelete', (pedId, key) => {
+        Ped.getByID(pedId).syncedMetaData[key] = undefined;
+        delete Ped.getByID(pedId).syncedMetaData[key];
+    });
+
+    //Event which fires if syncedMeta of a ped has been changed
+    alt.onServer('pedSyncer:server:metaSet', (pedId, key, value) => {
+        Ped.getByID(pedId).syncedMetaData[key] = value;
+    });
+
+    //Event which fires if a ped was created on server-side or on connect of this player
+    alt.onServer('pedSyncer:server:create', (newPed) => {
+        if (typeof Ped.getByID(newPed.id) !== "undefined") return;
+        let ped = new Ped(newPed);
+    });
+
+    //Event which fires if a ped was deleted
+    //Currently inactive
+    alt.onServer('pedSyncer:server:delete', (ped) => {
+
+    });
+
+    //Event which fires if the properties of a ped has been changed
+    alt.onServer('pedSyncer:server:update', (ped) => {
+        Ped.getByID(ped.id).updateProperties(ped);
+    });
+
+    alt.onServer("entitySync:create", (entityId, entityType, position, newEntityData) => {
+        if (entityType != pedType) return;
+        trySpawn(entityId, position, 0, 100);
+    });
+
+    function trySpawn(entityId, position, spawnTrys, spawnTrysTime) {
+        if (spawnTrys >= 10) return;
+        let ped = Ped.getByID(entityId);
+
+        if (typeof ped === "undefined" || typeof ped.id === "undefined" || ped.id != entityId) {
+            alt.setTimeout(() => {
+                trySpawn(entityId, position, spawnTrys+1, spawnTrysTime*2);
+            }, spawnTrysTime);
+        } else {
+            ped.pedSpawnTrys = 0;
+            ped.pedSpawnTryTime = 100;
+            ped.pos = position;
+            ped.spawn();
+        }
+    }
+
+    alt.onServer("entitySync:remove", (entityId, entityType) => {
+        if (entityType != pedType) return;
+        Ped.getByID(entityId).outOfRange();
+    });
+
+    alt.onServer("entitySync:updateData", (entityId, entityType, newEntityData) => {
+        if (entityType != pedType) return;
+        if (newEntityData["netOwner"] == alt.Player.local.id) setNetOwner(entityId, 0, 100);
+    });
+
+    function setNetOwner(entityId, setTrys, setTrysTime) {
+        if (setTrys >= 10) return;
+        let ped = Ped.getByID(entityId);
+
+        if (typeof ped === "undefined") {
+            alt.setTimeout(() => {
+                setNetOwner(entityId, setTrys+1, setTrysTime*2);
+            }, setTrysTime);
+        } else {
+            ped.becomeNetOwner();
+        }
+    }
+    
+    //Event which fires if a ped has a new path
+    alt.onServer('pedSyncer:server:path', (pedId, path) => {
+        Ped.getByID(pedId).setPath(path);
+    });
+
+    //Delete all current peds if the ressource stop (important for development)
+    alt.on("resourceStop", () => {
+        for (let ped of Ped.getAllStreamedPeds()) native.deletePed(ped.scriptID);
+    });
+}
